eb783520e75bf26ca3656efc737a12be5896ed99
LeKhoi <kudobkpro@gmail.com>
2018-01-10 17:40:56 +0700

    commit test controller

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+  
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        public static string[] a;
+        public IActionResult Index()
+        {
+            return Ok(a);
+        }
+    }
+}
