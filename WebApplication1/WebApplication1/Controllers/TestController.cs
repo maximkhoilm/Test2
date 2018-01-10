43c3d061af242c19f5207c960830a20d02485e58
LeKhoi <kudobkpro@gmail.com>
2018-01-10 17:46:46 +0700

    Commit test list

--- WebApplication1/WebApplication1/Controllers/TestController.cs
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -11,10 +11,18 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")]
     public class TestController : Controller
     {
-        public static string[] a;
+        
         public IActionResult Index()
         {
-            return Ok(a);
+            List<Test> _ls = new List<Test>();
+            _ls.Add(new Test { ID = 1, Name = "A" });
+            return Ok(_ls);
         }
+    }
+
+    public class Test
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
